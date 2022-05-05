725417fa2e1737d7b6be3504757ba8ac51e2c814
Daichi Suzuki <ds.81ueman@gmail.com>
2022-05-06 00:18:58 +0900

    readable variable name

--- README.md
+++ README.md
@@ -6,73 +6,86 @@ Béchet, Denis, Annie Foret, and Isabelle Tellier. 2007. “Learnability of Preg
 feature-tagged exampleを入力としてCPSソルバーsugar用のインプットファイル(.cps)を作成し解かせてから入力の文章に対するパースを表示する。
 
 
-## 入力
+## 入力ファイル
 ```
 (word type+ ;)+
 ```
-## example
+## example(input.txt)
 ```
 he pi_3;loves s_1 pi_3 n;small n n;cats n 
+```
+## 
+``` 
+cargo run --bin bft-pg input.txt > hoge.cps
 ```
 
 ## 出力例
 
 ```
-(int x_0_0 1 1)
-(int u_0_0 -3 3)
-(int x_1_0 1 3)
-(int u_1_0 -3 3)
-(int x_1_1 1 3)
-(int u_1_1 -3 3)
-(int x_1_2 1 3)
-(int u_1_2 -3 3)
-(int x_2_0 1 2)
-(int u_2_0 -3 3)
-(int x_2_1 1 2)
-(int u_2_1 -3 3)
-(int x_3_0 1 1)
-(int u_3_0 -3 3)
+(int x_he_0_0 1 1)
+(int u_he_0_0 -3 3)
+(int x_loves_1_0 1 3)
+(int u_loves_1_0 -3 3)
+(int x_loves_1_1 1 3)
+(int u_loves_1_1 -3 3)
+(int x_loves_1_2 1 3)
+(int u_loves_1_2 -3 3)
+(int x_small_2_0 1 2)
+(int u_small_2_0 -3 3)
+(int x_small_2_1 1 2)
+(int u_small_2_1 -3 3)
+(int x_cats_3_0 1 1)
+(int u_cats_3_0 -3 3)
+(< x_small_2_0 x_small_2_1)
 (int cost 0 1000)
-(= cost (+ (abs u_0_0) (abs u_1_0) (abs u_1_1) (abs u_1_2) (abs u_2_0) (abs u_2_1) (abs u_3_0) ))
+(= cost (+ (abs u_he_0_0) (abs u_loves_1_0) (abs u_loves_1_1) (abs u_loves_1_2) (abs u_small_2_0) (abs u_small_2_1) (abs u_cats_3_0) ))
 (objective minimize cost)
-(< x_1_1 x_1_0)
-(< x_1_1 x_1_2)
-(= (+ u_0_0 1) u_1_1)
-(< x_1_0 x_1_2)
-(< x_2_0 x_2_1)
-(= (+ u_1_2 1) u_2_0)
-(= (+ u_2_1 1) u_3_0)
+(< x_loves_1_1 x_loves_1_0)
+(< x_loves_1_1 x_loves_1_2)
+(= (+ u_he_0_0 1) u_loves_1_1)
+(< x_loves_1_0 x_loves_1_2)
+(< x_small_2_0 x_small_2_1)
+(= (+ u_loves_1_2 1) u_small_2_0)
+(= (+ u_small_2_1 1) u_cats_3_0)
 ```
 
 sugar に投げると
 ```
 s SATISFIABLE
-o 4
-a x_1_0 2
-a x_2_0 2
-a x_2_1 0
-a x_2_2 2
-a x_3_0 2
-a u_1_0 1
-a u_2_0 2
-a u_2_1 0
-a u_2_2 0
-a u_3_0 1
-a cost  4
+o 7
+a x_he_0_0      1
+a u_he_0_0      -1
+a x_loves_1_0   2
+a u_loves_1_0   0
+a x_loves_1_1   1
+a u_loves_1_1   0
+a x_loves_1_2   3
+a u_loves_1_2   2
+a x_small_2_0   1
+a u_small_2_0   3
+a x_small_2_1   2
+a u_small_2_1   -1
+a x_cats_3_0    1
+a u_cats_3_0    0
+a cost  7
 a
-o 2
-a x_1_0 1
-a x_2_0 1
-a x_2_1 0
-a x_2_2 1
-a x_3_0 1
-a u_1_0 0
-a u_2_0 1
-a u_2_1 0
-a u_2_2 -1
-a u_3_0 0
-a cost  2
+o 3
+a x_he_0_0      1
+a u_he_0_0      0
+a x_loves_1_0   2
+a u_loves_1_0   0
+a x_loves_1_1   1
+a u_loves_1_1   1
+a x_loves_1_2   3
+a u_loves_1_2   0
+a x_small_2_0   1
+a u_small_2_0   1
+a x_small_2_1   2
+a u_small_2_1   0
+a x_cats_3_0    1
+a u_cats_3_0    1
+a cost  3
 a
 s OPTIMUM FOUND
 ```
-というように帰ってくるのでこれを元に文法を復元する。
+というように帰ってくるのでこれを元に文法を復元する(したい)。
--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,18 @@ use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 
-struct Tagged{
+struct TaggedWord{
+    word: String,
     pos: usize,
     types: Vec<(usize,String)>
 }
+
+type Sentence = Vec<TaggedWord>;
+
+fn name(st: &str) -> &str{
+    st.split_ascii_whitespace().next().unwrap().trim()
+}
+
 
 fn main() {
     let args:Vec<String> = env::args().collect();
@@ -23,71 +31,65 @@ fn main() {
     assert_eq!(1, pnum%2);
     
 
-    let mut a: Vec<String>  = Vec::new();
-    let mut ty_arr: Vec<Tagged> = Vec::new(); //usize: original position in the sentence
+    let mut ty_arr: Sentence = Vec::new(); //usize: original position in the sentence
 
     //constraints in step 0
     for (i,s) in contents.iter().enumerate(){
         let n = s.split_ascii_whitespace().skip(1).count();
-        ty_arr.push(Tagged {pos:i,types:Vec::new()});
+        ty_arr.push(TaggedWord {word:name(s).to_string(), pos:i,types:Vec::new()});
+        let word = name(s);
         for (j,typ) in s.split_ascii_whitespace().skip(1).enumerate(){
-            println!("(int x_{}_{} 1 {})", i,j, n);
-            println!("(int u_{}_{} -3 3)", i,j);
-            a.push(s.to_string());
+            println!("(int x_{}_{}_{} 1 {})", word,i,j, n);
+            println!("(int u_{}_{}_{} -3 3)", word,i,j);
             ty_arr[i].types.push((j,typ.to_string()));
         }
     }
     for v in ty_arr.iter(){
         for (j,s) in v.types.iter().enumerate() {
-            if let Some(pos) = v.types.iter().skip(j+1).position(|t| s==t) {
-                println!("(< x_{}_{} x_{}_{})", v.pos,j,v.pos,pos+j+1); //なんかもっとキレイな書き方あるでしょ
+            if let Some(pos) = v.types.iter().skip(j+1).position(|t| s.1==t.1) {
+                println!("(< x_{}_{}_{} x_{}_{}_{})", v.word ,v.pos,j,v.word, v.pos,pos+j+1); //なんかもっとキレイな書き方あるでしょ
             }
         }
     }
     println!("(int cost 0 1000)");
     print!("(= cost (+ ");
-    for Tagged{pos:i,types:v} in ty_arr.iter(){
+    for TaggedWord{word,pos,types:v} in ty_arr.iter(){
         for (j,_) in v.iter() {
-            print!("(abs u_{}_{}) ", i,j);
+            print!("(abs u_{}_{}_{}) ", word,pos,j);
         }
     }
     println!("))");
     println!("(objective minimize cost)");
     // external
     while pnum!=1 {
-        let mut rn = 0;
-        let mut ri0 = 0;
-        let mut rm = 0;
-        let mut rj0 = 0;
-        let mut found = false;
-        for Tagged {pos:_, types:v} in ty_arr.iter() {
+        let mut rmv_ind = None;
+        for TaggedWord { types:v,..} in ty_arr.iter() {
             eprintln!("{:?}", v);
         }
-        'outer: for Tagged{pos:n,types:v} in ty_arr.iter().take(ty_arr.len()-1) {
-            let Tagged{pos:m, types:w} = &ty_arr[n+1];
+        'outer: for TaggedWord{word:word1,pos:n,types:v} in ty_arr.iter().take(ty_arr.len()-1) {
+            let TaggedWord{word:word2,pos:m, types:w} = &ty_arr[n+1];
             for (ipos,(i0,t1))in v.iter().enumerate().rev() {
                 if let Some(jpos) = w.iter().position(|(_,t2)| t1 == t2) {
                     let (j0,_) = &w[jpos];
                     eprintln!("found pair! {} {} {} {}", n,i0, m, j0);
-                    rn = *n;
-                    ri0 = ipos;
-                    rm = *m;
-                    rj0 = jpos;
-                    for (i,_) in v {if i!=i0 {println!("(< x_{}_{} x_{}_{})", n,i, n,i0);}}
-                    for (j,_) in w {if j!=j0 {println!("(< x_{}_{} x_{}_{})", m,j0, m,j)}}
-                    println!("(= (+ u_{}_{} 1) u_{}_{})", n,i0, m,j0);
+                    rmv_ind = Some((*n,ipos,*m,jpos));
+                    for (i,_) in v {if i!=i0 {println!("(< x_{}_{}_{} x_{}_{}_{})", word1, n,i,word1, n,i0);}}
+                    for (j,_) in w {if j!=j0 {println!("(< x_{}_{}_{} x_{}_{}_{})", word2, m,j0, word2,m,j)}}
+                    println!("(= (+ u_{}_{}_{} 1) u_{}_{}_{})", word1, n,i0,word2, m,j0);
                     pnum -= 2;
-                    found = true;
                     break 'outer;
                 }
             }
         }
-        if found {
-            ty_arr[rn].types.remove(ri0);
-            ty_arr[rm].types.remove(rj0);
-        } else {
-            eprintln!("cannot finish external contraction");
-            std::process::exit(1);
+        match rmv_ind {
+            Some((rn,ri0,rm,rj0)) => {
+                ty_arr[rn].types.remove(ri0);
+                ty_arr[rm].types.remove(rj0);
+            },
+            None => {
+                eprintln!("cannot finish external contraction");
+                std::process::exit(1);
+            }
         }
     }
 
